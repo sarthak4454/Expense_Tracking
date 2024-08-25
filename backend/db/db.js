14f0fb4fc27742eb72321477f7b733f0a8f192a0
Sarthak Garg <92808008+sarthak4454@users.noreply.github.com>
2024-08-25 12:58:48 +0530

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const db = async () => {
     try {
         mongoose.set('strictQuery', false)
-        await mongoose.connect("mongodb+srv://sarthak_garg4454:sarthak@cluster3.12tcdva.mongodb.net/?retryWrites=true&w=majority")
+        await mongoose.connect("mongodb+srv://sarthak_garg4454:sarthak123@cluster3.12tcdva.mongodb.net/?retryWrites=true&w=majority&appName=Cluster3")
         console.log('Db Connected')
     } catch (error) {
-        console.log('DB Connection Error');
+        console.log('DB Connection Error',error);
     }
 }
 
